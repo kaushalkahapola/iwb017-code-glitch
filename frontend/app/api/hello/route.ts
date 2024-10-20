c407d18348afb6436a72e2a23fc0e4e4af48a5ea
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-10-20 15:17:05 +0530

    Created Home Page and User Dashboard

--- frontend/app/api/hello/route.ts
+++ frontend/app/api/hello/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const API_URL = process.env.API_URL;
-
 export async function GET(request: NextRequest) {
-    const response = await fetch(`${API_URL}/greeting`);
-    const data = await response.text();
-    console.log(data);
-    return NextResponse.json(data);
+    const response = "Hello from Next.js!";
+    return NextResponse.json(response);
 }
